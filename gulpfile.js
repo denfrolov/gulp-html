ba688c22460e70bcf2479e67f0b3f464eeac321f
Denis <den-frolov38@yandex.ru>
2019-04-14 11:05:39 +0300

    c

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
-var syntax = 'sass'; // Syntax: sass or scss;
-
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	jade = require('gulp-jade'),
 	sass = require('gulp-sass'),
 	browsersync = require('browser-sync'),
 	concat = require('gulp-concat'),
@@ -10,8 +7,7 @@ var gulp = require('gulp'),
 	cleancss = require('gulp-clean-css'),
 	rename = require('gulp-rename'),
 	autoprefixer = require('gulp-autoprefixer'),
-	notify = require("gulp-notify"),
-	rsync = require('gulp-rsync');
+	notify = require("gulp-notify");
 
 gulp.task('browser-sync', function () {
 	browsersync({
@@ -25,19 +21,8 @@ gulp.task('browser-sync', function () {
 	})
 });
 
-gulp.task('jade', function () {
-	return gulp.src('app/jade/*.jade')
-		.pipe(jade({
-			pretty: true
-		}).on("error", notify.onError()))
-		.pipe(gulp.dest('app'))
-		.pipe(browsersync.reload({
-			stream: true
-		}));
-});
-
 gulp.task('styles', function () {
-	return gulp.src('app/' + syntax + '/**/*.' + syntax + '')
+	return gulp.src('app/css/main.scss')
 		.pipe(sass({
 			outputStyle: 'expand'
 		}).on("error", notify.onError()))
@@ -59,10 +44,25 @@ gulp.task('styles', function () {
 		}))
 });
 
+gulp.task('libs-styles', function () {
+	return gulp.src([
+		'app/libs/bootstrap/dist/css/bootstrap.min.css'
+	])
+		.pipe(concat('libs.min.css'))
+		.pipe(cleancss({
+			level: {
+				1: {
+					specialComments: 0
+				}
+			}
+		})) // Opt., comment out when debugging
+		.pipe(gulp.dest('app/libs'))
+});
+
 gulp.task('js', function () {
 	return gulp.src([
-			'app/libs/jquery/dist/jquery.min.js',
-		])
+		'app/libs/jquery/dist/jquery.min.js',
+	])
 		.pipe(concat('scripts.min.js'))
 		.pipe(uglify()) // Mifify js (opt.)
 		.pipe(gulp.dest('app/js'))
@@ -71,26 +71,10 @@ gulp.task('js', function () {
 		}))
 });
 
-gulp.task('rsync', function () {
-	return gulp.src(["app/**/*", "!app/libs/**/*"])
-		.pipe(rsync({
-			root: 'app/',
-			hostname: 'username@yousite.com',
-			destination: 'yousite/public_html/',
-			// include: ['*.htaccess'], // Includes files to deploy
-			exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
-			recursive: true,
-			archive: true,
-			silent: false,
-			compress: true
-		}))
-});
-
 gulp.task('watch', function () {
 	gulp.watch('app/' + syntax + '/**/*.' + syntax + '', gulp.parallel('styles'));
 	gulp.watch('app/js/*.js', browsersync.reload);
-	gulp.watch('app/jade/**/*.jade', gulp.parallel('jade'));
-	//gulp.watch('app/*.html', browsersync.reload)
+	gulp.watch('app/*.html', browsersync.reload)
 });
 
-gulp.task('default', gulp.parallel('watch', 'jade', 'styles', 'js', 'browser-sync'));
+gulp.task('default', gulp.parallel('watch', 'libs-styles', 'styles', 'js', 'browser-sync'));
